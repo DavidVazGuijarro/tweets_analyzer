2c369c04bd682d7c017c984288d5d728f8146564
David Vaz Guijarro <vazdavid@icloud.com>
2017-06-07 22:53:58 +0200

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,9 +1,9 @@
 # Go to http://dev.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
-consumer_key="xxxxxxxxxxxxxx"
-consumer_secret="xxxxxxxxxxxxx"
+consumer_key="uGVC7TcsGAwiVBAJ56jh1hVek"
+consumer_secret="OPbof0cjFBwek4BROOoZhUi5fknaKoB3BMy7yWbsmSGNXCSCr8"
 
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
-access_token="xxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxx"
-access_token_secret="xxxxxxxxxxxxxxxxxxxxxxx"
+access_token="167201584-AGD3Z15ukkz34fEMRrCAMCw5a1TLB9Y0RHUAgFxQ"
+access_token_secret="w76bWSF6NdQV1Jh5kDEHt4ndknv1UlZKz2h0neEpw2EVy"
